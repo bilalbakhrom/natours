a65491dbf90869bf90ab55df3c0129f6926610ca
Bilal <bilalbakhrom@gmail.com>
2024-03-25 07:22:02 +0500

    feat: return tour by id

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = 3000;
 
 app.use(express.json());
 
-const toursJSON = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const toursJSON = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -18,20 +20,49 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > toursJSON.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = toursJSON.find((item) => item.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = toursJSON[toursJSON.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.assign(
+    {
+      id: newId,
+    },
+    req.body
+  );
 
   toursJSON.push(newTour);
 
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(toursJSON), (err) => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  });
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursJSON),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
