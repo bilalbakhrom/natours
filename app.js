0b773f5eb0783f888b7042b87d1ab692e8cc1d27
Bilal <bilalbakhrom@gmail.com>
2024-03-25 07:07:28 +0500

    feat: add post api to add a new tour

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const fs = require('node:fs');
 const app = express();
 const port = 3000;
 
-const toursJSON = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+app.use(express.json());
+
+const toursJSON = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -13,6 +15,22 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours: toursJSON,
     },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = toursJSON[toursJSON.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  toursJSON.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(toursJSON), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
