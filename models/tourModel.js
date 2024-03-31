666a84923f434676fc62a160b01e7c80686c1083
Bilal <bilalbakhrom@gmail.com>
2024-03-31 07:37:57 +0500

    feat: update tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,8 +5,26 @@ const tourSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, 'The tour name must be specified'],
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    trim: true,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0.0,
+  },
+  ratingsQuantity: {
     type: Number,
     default: 0.0,
   },
@@ -14,6 +32,26 @@ const tourSchema = mongoose.Schema({
     type: Number,
     required: [true, 'The tour price must be specified'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
