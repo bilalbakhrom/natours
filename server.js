3d002a21b3e57b5401d2267c6ed2ececd3a7630e
Bilal <bilalbakhrom@gmail.com>
2024-03-31 06:12:33 +0500

    feat: add mongoose

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
+const database = process.env.DATABASE_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+).replace('<USERNAME>', process.env.DATABASE_USERNAME);
+
 const app = require('./app');
+
+mongoose
+  .connect(database)
+  .then(() => {
+    console.log('Database connection succesfful');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
