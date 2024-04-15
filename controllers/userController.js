95b0d52067a5fca4f105849cab574f65380560c9
Bilal <bilalbakhrom@gmail.com>
2024-04-15 12:34:18 +0500

    feat: encrypt user sensative data

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,15 +26,6 @@ const getUser = catchAsync(async (req, res, next) => {
   });
 });
 
-const createUser = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
-
-  res.status(200).json({
-    status: 'success',
-    data: { user },
-  });
-});
-
 const updateUser = catchAsync(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -70,7 +61,6 @@ const deleteUser = catchAsync(async (req, res, next) => {
 module.exports = {
   getAllUsers,
   getUser,
-  createUser,
   updateUser,
   deleteUser,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -25,11 +26,24 @@ const userSchema = mongoose.Schema(
     passwordConfirm: {
       type: String,
       required: [true, 'Confirm password must be specified'],
-      minlength: 8,
+      validate: {
+        validator: function (el) {
+          return this.password == el;
+        },
+        message: 'Passwords are not the same',
+      },
     },
   },
   { versionKey: false },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
